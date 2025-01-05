ca31221741e2b8f732b47654891e4c15e8c263a7
askdgeju <104259284+askdgeju@users.noreply.github.com>
2025-01-05 10:23:18 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || 'd124b882-f060-4b05-81fa-9dd588c616c0'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || 'N9BVoBfucVIrIzCBt8';   // 没用哪吒，把这个参数空着
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || '';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'forchofty.vxfsudw.eu.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMjZhODQ5NjlhZGI3ODYzNGI2YWM3ZGM3MGIyN2EwNzYiLCJ0IjoiZGU2ZGZmYzAtYmU2My00M2UzLTllNGItYmUwOGY2NzU5N2ZlIiwicyI6Ik0yTXlOVGcyWm1FdFpHWmxOeTAwWVdZMkxUaGlOakV0WTJNMlkyWTBZalE1TURVeiJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
